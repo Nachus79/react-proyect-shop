b601292bbe128741ea9e03e5bea3a87a616451ba
Nacho Monzón <ignaciomonzona@gmail.com>
2024-12-21 00:12:40 +0100

    Arregladas las cartas

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-/*
+
 const API_KEY = import.meta.env.VITE_API_KEY || "http://localhost:8000";
-*/
-const API_KEY = "08ce5badfd794c18bd5b24fe9c325fcf";
+
 
 export const fetchGames = async (page = 1, pageSize = 40) => {
   const response = await axios.get(
